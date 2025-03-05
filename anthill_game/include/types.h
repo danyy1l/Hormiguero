ef840840cdc8a90ac6f1938a5bbd8100cf46ce4d
danyyil <danyyil.shykerynets@estudiante.uam.es>
2025-03-05 23:51:16 +0100

    Recuperado types

---
+++ anthill_game/include/types.h
@@ -0,0 +1,31 @@
+/**
+ * @brief It defines common types for the whole project
+ *
+ * @file types.h
+ * @author Anthony Eduardo Alvarado Carbajal
+ * @version 7
+ * @date 04-02-2025
+ * @copyright GNU Public License
+ */
+
+#ifndef TYPES_H
+#define TYPES_H
+
+#define WORD_SIZE 1000
+#define GDESC_SIZE 10
+#define GDESC_MAX 5
+#define STATUS_SIZE 6
+#define NO_ID -1
+
+#define CHARACTERS_NUM 3
+#define OBJECTS_NUM 4
+
+typedef long Id;
+
+typedef enum { FALSE, TRUE } Bool;
+
+typedef enum { ERROR, OK } Status;
+
+typedef enum { N, S, E, W } Direction;
+
+#endif
