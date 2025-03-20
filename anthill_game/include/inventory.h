b4c89cb4ea665e0c1a8f5a828d1195a4d13b2100
alvarotaku59 <alvaro.rodriguezh@estudiante.uam.es>
2025-03-20 15:56:12 +0100

    primero version del inventory comentado

--- anthill_game/include/inventory.h
+++ anthill_game/include/inventory.h
@@ -17,17 +17,86 @@
  #include "types.h"
  #include "set.h"
  
+  /**
+ * @brief Estructura Inventory con campos:
+ * array de objects, int maximo de objetos 
+ * @author Danyyil Shykerynets
+ */
  typedef struct _Inventory Inventory;
 
+ /**
+ * @brief Crea un inventario nuevo
+ * @author Álvaro Rodríguez
+ * @param 
+ * @return Devuelve puntero a la nueva estrucura invebtory en caso de exito y NULL en caso contrario
+ */
  Inventory *inventory_create();
+  /**
+ * @brief destruye un inventario 
+ * @author Álvaro Rodríguez
+ * @param Inventory puntero al inventario
+ * @return Devuelve OK en caso de exito y ERROR en caso contrario
+ */
  Status inventory_destroy(Inventory * inventory);
+   /**
+ * @brief comprueva si el inventario esta vacio
+ * @author Álvaro Rodríguez
+ * @param Inventory puntero al inventario
+ * @return Devuelve TRUE en caso de que este vacio y FALSE en caso contrario
+ */
  Bool inventory_is_empty(Inventory * inventory);
+    /**
+ * @brief comprueva si el inventario esta lleno
+ * @author Álvaro Rodríguez
+ * @param Inventory puntero al inventario
+ * @return Devuelve TRUE en caso de que este lleno y FALSE en caso contrario
+ */
  Bool inventory_is_full(Inventory * inventory);
+    /**
+ * @brief añade un objeto al inventario
+ * @author Álvaro Rodríguez
+ * @param Inventory puntero al inventario
+ * @param Id id del objeto
+ * @return Devuelve OK en caso de exito y ERROR en caso contrario
+ */
  Status inventory_add_object(Inventory* inventory, Id id);
+     /**
+ * @brief borra un objeto del inventario
+ * @author Álvaro Rodríguez
+ * @param Inventory puntero al inventario
+ * @param Id id del objeto
+ * @return Devuelve OK en caso de exito y ERROR en caso contrario
+ */
  Status inventory_del_object(Inventory* inventory, Id id);
+     /**
+ * @brief verifica que un objeto este en el inventario 
+ * @author Álvaro Rodríguez
+ * @param Inventory puntero al inventario
+ * @param Id id del objeto
+ * @return Devuelve TRUE en caso de que este en el inventario y FALSE en caso contrario
+ */
  Bool inventory_find_object(Inventory* inventory,Id id);
+     /**
+ * @brief modifica el maximo de objetos del inventario
+ * @author Álvaro Rodríguez
+ * @param Inventory puntero al inventario
+ * @param int nuevo maximo
+ * @return Devuelve OK en caso de exito y ERROR en caso contrario
+ */
  Status inventory_set_max_objects(Inventory *inventory,int n);
+     /**
+ * @brief recoje el maximo de objetos del inventario
+ * @author Álvaro Rodríguez
+ * @param Inventory puntero al inventario
+ * @return Devuelve el maximo de objetos del inventario en caso de exito y -1 en caso contrario
+ */
  int inventory_get_max_objects(Inventory *inventory);
+     /**
+ * @brief imprime las caracteristicas del inventario
+ * @author Álvaro Rodríguez
+ * @param Inventory puntero al inventario
+ * @return Devuelve OK en caso de exito y ERROR en caso contrario
+ */
  Status inventory_print(Inventory* inventory);
 
 
