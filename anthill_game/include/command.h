4c69755cb32b9f3c75f725e19588cea7857c26c4
danyyil <danyyil.shykerynets@estudiante.uam.es>
2025-03-25 20:40:32 +0100

    Una tonteria

--- anthill_game/include/command.h
+++ anthill_game/include/command.h
@@ -52,7 +52,6 @@ Status command_destroy(Command* command);
  * @brief Define el código para el comando
  * @author Anthony Eduardo Alvarado Carbajal
  * @param command un puntero a Command 
- *
  * @param code un valor de Commandcode, ya sea NO_CMD,UNKNOWN,EXIT,MOVE,TAKE,DROP,ATTACK,CHAT,INSPECT
  * @return Ok si todo va bien o ERROR si hay un fallo
 */
@@ -62,7 +61,7 @@ Status command_set_code(Command* command, CommandCode code);
  * @brief Obtiene un codigo a través del comando
  * @author Anthony Eduardo Alvarado Carbajal
  * @param command un puntero a Command
- * @return NO_CMD,UNKNOWN,EXIT,NORTH,EAST,SOUTH,WEST
+ * @return NO_CMD,UNKNOWN,EXIT,MOVE,TAKE,DROP,ATTACK,CHAT,INSPECT
  */
 CommandCode command_get_code(Command* command);
 
