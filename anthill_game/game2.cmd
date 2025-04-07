ffdc770c9de3b87848c9d62e29f3f5f951ecce84
danyyil <danyyil.shykerynets@estudiante.uam.es>
2025-04-07 10:59:34 +0200

    Pruebas cmd done

---
+++ anthill_game/game2.cmd
@@ -0,0 +1,19 @@
+move south
+move west
+take crumb
+move south
+move south
+move west
+move south
+move north
+chat
+take crumb
+attack
+move north
+move south
+chat
+drop cookie
+move south
+drop crumb
+take cookie
+quit
